11d7387832bfc77cefa61934cdcb5d861cf0c17a
Carlos Velazquez <skamateria@gmail.com>
2019-06-05 06:21:33 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,5 +3,4 @@ def using_push(countries_in_western_africa, next_country)
   next_country = "Niger"
   updated_array = countries_in_western_africa.push(next_country)
   end
-  return updated_array
-end
+
