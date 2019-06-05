fedfa9889abac43137d040815bc2e280154bed8e
Carlos Velazquez <skamateria@gmail.com>
2019-06-05 06:27:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -5,8 +5,8 @@ def using_push(countries_in_western_africa, next_country)
   end
 
 
-def using_unshift(neighborhoods_in_northwest_brooklyn, @new_neighborhood)
+def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood)
   neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
-  @new_neighborhood = "Brooklyn Heights"
-  @updated_array = neighborhoods_in_northwest_brooklyn.unshift(@new_neighborhood)
+  new_neighborhood = "Brooklyn Heights"
+  updated_array = neighborhoods_in_northwest_brooklyn.unshift(new_neighborhood)
 end
