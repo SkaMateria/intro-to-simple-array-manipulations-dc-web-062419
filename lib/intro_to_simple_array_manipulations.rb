f1ef1dcf98e3a3f25d38fa3adc52b07959917d98
Carlos Velazquez <skamateria@gmail.com>
2019-06-05 06:13:18 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,6 @@
+ref using_push(array, string)
+  countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
+  next_country = countries_in_western_africa.push("Niger")
+  updated_array = next_country
+end
+end
