4efbaf77e440a5963e519932b982b7f96a089dba
Carlos Velazquez <skamateria@gmail.com>
2019-06-05 06:29:38 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,3 +10,9 @@ def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood)
   new_neighborhood = "Brooklyn Heights"
   updated_array = neighborhoods_in_northwest_brooklyn.unshift(new_neighborhood)
 end
+
+
+def using_pop(great_hits_of_the_nineties)
+  great_hits_of_the_nineties = []
+  deleted_string = great_hits_of_the_nineties.pop
+end
